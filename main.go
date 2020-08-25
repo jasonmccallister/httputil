4bbd838aacbc8d90d9fe319ba8beca84875be890
Jason McCallister <jason@craftcms.com>
2020-08-25 16:41:12 -0400

    type-o
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- main.go
+++ main.go
@@ -120,7 +120,7 @@ func envHandler(w http.ResponseWriter, r *http.Request) {
 
 func postgresHandler(w http.ResponseWriter, r *http.Request) {
 	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_SERVER"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
-	db, err := sql.Open("pgqsl", conn)
+	db, err := sql.Open("pgsql", conn)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
